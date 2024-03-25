d01beec243dd3112586343e9fb37c99af5e1c508
jimatlang <jim.stewart@thelangschool.org>
2024-03-25 14:00:31 -0400

    conrad's code

--- Sorter.java
+++ Sorter.java
@@ -5,6 +5,26 @@ class Sorter{
       arr[m] = temp;
     }
     public void sort(int[] arr){
-        
+        int n = 0;
+        int last = arr.length-1;
+        boolean sorted = false;
+        int sort = 0;
+        while(!sorted){
+          
+          while (n < last){
+            if(arr[n] > arr[n+1]){
+              swap(arr,n,n+1);
+            } else {
+              sort++;
+            }
+            n++;
+          }
+          if(sort == last-1){
+            sorted = true;
+          }
+
+          n = 0;
+          sort = 0;
+        }
     }
 }  
