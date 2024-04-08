82b3c6bbb5753b73f4cbaa33adbc433be11a9819
Zavier Gaffin <zavier.gaffin@thelangschool.net>
2024-04-08 13:55:36 -0400

    sorter code start

--- Sorter.java
+++ Sorter.java
@@ -5,17 +5,8 @@ class Sorter{
       arr[m] = temp;
     }
     public void sort(int[] arr){
-        int n = 0;
-        int last = arr.length-1;
-        //boolean sorted = false;
-        //while(!sorted){
-          while(n < last){
-            if(arr[n] > arr[n+1]){
-              swap(arr, n, n+1);
-            }
-            n++;
-          }
-          n = 0;
-        //}
+      for(int i = 0; i<arr.length;i++){
+
+      }  
     }
 }  
